3224fade9f3da63d00fb9824740d1273596d0cc4
Andrii Taranenko <andrii.taranenko@lp-praha.cz>
2022-12-28 21:02:44 +0100

    Rem generic

--- src/theme/emotion.d.ts
+++ src/theme/emotion.d.ts
@@ -3,10 +3,14 @@ import "@emotion/react";
 import { TColors, TDevices, TFonts } from "./variables/types";
 
 declare module "@emotion/react" {
+  interface TRem<P = number> {
+    (px: P): string;
+  }
+
   export interface Theme {
     colors: TColors;
     devices: TDevices;
     fonts: TFonts;
-    rem: (px: number) => string;
+    rem: TRem;
   }
 }
